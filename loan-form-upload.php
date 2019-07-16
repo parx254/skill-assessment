209e27c393857fc799c54321e124460a6c0ee733
Parker Austin McClelland <49407804+parx254@users.noreply.github.com>
2019-07-15 23:04:03 -0400

    Update loan-form-upload.php

--- loan-form-upload.php
+++ loan-form-upload.php
@@ -11,5 +11,5 @@ $to = "parx317@gmail.com";
 $headers = "From: $email_from \r\n";
 $headers .= "Reply To: $visitor_email \r\n";
 mail($to,email_subject,$email_body,$headers);
-header("Location: index.html");
+header("Location: success.html");
 ?>
